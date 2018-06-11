c33302871c6e9e8baffc209d445e3fc6246cd935
Nurmister <34708604+nurmister@users.noreply.github.com>
2018-06-11 22:10:56 +0800

    Update takephoto.sh

--- pi/takephoto.sh
+++ pi/takephoto.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATE=$(date +"%Y-%m-%d-%H%M")
-raspistill --nopreview --exposure auto --awb auto --saturation -25 --metering average --quality 100 --exif none --timeout 10 --output /home/pi/stills/$DATE.jpg
+raspistill --nopreview --exposure auto --awb auto --saturation -25 --metering average --quality 100 --exif none --timeout 10 --output /media/pi/pictures/$DATE.jpg
