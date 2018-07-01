c306a7ee659e6ea56129d4cf04e5ef4d8270704e
nurmister <nurmister@protonmail.com>
2018-07-01 21:31:50 +0800

    m

--- labelling/label_spots/label_checker.R
+++ labelling/label_spots/label_checker.R
@@ -2,8 +2,8 @@ library(imager)
 library(stringr)
 
 directory <- "label_pictures/"
-date <- readline("Enter date: ")
-time_started <- readline("Enter start time: ")
+date <- "2018-06-20"
+time_started <- "1356"
 file_names <- list.files(directory, pattern="*.jpg")
 labels <- read.csv(paste("label_csvs/", date, "_", time_started, ".csv", sep=""))
 par(mfrow=c(1, 2))
@@ -11,19 +11,23 @@ for (index in 1:length(file_names)) {
   image <- load.image(paste(directory, file_names[index], sep=""))
   plot(image,
        main=file_names[index])
-  plot(c(0, 1),
-       c(0, 1),
+  plot(c(-1, 3),
+       c(-1, 3),
        ann=FALSE,
        bty="n",
        type="n",
        xaxt="n",
        yaxt="n")
-  text(x=0,
-       y=0.5,
+  text(x=-1,
+       y=1,
        labels$label[labels$date_id == 
                       str_sub(file_names[index],
                               1,
                               length(file_names[index]) - 6)], 
+       cex = 1.6, col = "black")
+  text(x=0.75,
+       y=1,
+       paste(index, "/", length(file_names), sep=""),
        cex = 1.6, col = "black")
   readline("")
 }
