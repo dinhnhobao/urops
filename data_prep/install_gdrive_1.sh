b55d0bf58a9c1ff21322beecdc65b873724fcdaf
nurmister <nurmister@protonmail.com>
2018-08-02 06:31:59 +0800

    Reset of repo.

---
+++ data_prep/install_gdrive_1.sh
@@ -0,0 +1,3 @@
+cd ~;
+wget https://docs.google.com/uc?id=0B3X9GlR6EmbnQ0FtZmJJUXEyRTA&export=download;
+echo rename the downloaded file gdrive;
---
+++ pi/README.md
@@ -0,0 +1,4 @@
+crontab -e
+append:  */5 * * * * /home/pi/takephoto.sh
+
+saves images to /home/pi/stills/ every five minutes
