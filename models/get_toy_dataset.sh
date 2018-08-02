fd1d9194adfa417ff0a6bd0978eb1f0cf5431db6
nurmister <nurmister@protonmail.com>
2018-08-03 00:25:52 +0800

    Changed to SKF from SSS

--- models/get_toy_dataset.sh
+++ models/get_toy_dataset.sh
@@ -1,6 +1,6 @@
 chmod +x gdown.pl;
-./gdown.pl https://drive.google.com/file/d/1WKlz6klTSA8CakQMCBI0UMq1dbMi-ZCp/view?usp=sharing test_set.h5;
-./gdown.pl https://drive.google.com/file/d/1Deg6Qy9JCGTuPP67eGR1nPP9k7a0TfJH/view?usp=sharing train_validation_set_1.h5;
-./gdown.pl https://drive.google.com/file/d/1Slo9J_LPOcPzGw7rH-zPaut-RYisL3Fb/view?usp=sharing train_validation_set_2.h5;
-./gdown.pl https://drive.google.com/file/d/1gUf7AAHl2euj73Y2GSiPJW8MmPF7kmxR/view?usp=sharing train_validation_set_3.h5;
+./gdown.pl https://drive.google.com/file/d/1WKlz6klTSA8CakQMCBI0UMq1dbMi-ZCp/view?usp=sharing test_set.hdf5;
+./gdown.pl https://drive.google.com/file/d/1Deg6Qy9JCGTuPP67eGR1nPP9k7a0TfJH/view?usp=sharing train_validation_set_1.hdf5;
+./gdown.pl https://drive.google.com/file/d/1Slo9J_LPOcPzGw7rH-zPaut-RYisL3Fb/view?usp=sharing train_validation_set_2.hdf5;
+./gdown.pl https://drive.google.com/file/d/1gUf7AAHl2euj73Y2GSiPJW8MmPF7kmxR/view?usp=sharing train_validation_set_3.hdf5;
 
