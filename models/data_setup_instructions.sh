ecc547cde1ec1205b5593e6e8e8dd72a78f5c841
nurmister <nurmister@protonmail.com>
2018-08-02 15:23:28 +0800

    Updated data-loading instructions.

---
+++ models/data_setup_instructions.sh
@@ -0,0 +1,7 @@
+echo 'To load data:
+1. (Install "gdrive") run and follow "install_gdrive_1.sh",
+   and then "install_gdrive_2.sh".
+2. (Download "h5.zip") use "gdrive" to download "h5.zip".
+3. (Unzip "h5.zip") in the directory where "h5.zip" was downloaded,
+   run "unzip h5.zip -d <path_to_data>", where "path_to_data"
+   is the path "cnn.py" loads data from.'
