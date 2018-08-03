7c18cfce679828783bf6eae89fdac3379f663e79
nurmister <nurmister@protonmail.com>
2018-08-03 12:56:43 +0800

    KFold CV working with model.

--- models/get_full_dataset.sh
+++ models/get_full_dataset.sh
@@ -1,6 +1,6 @@
+mkdir data;
 chmod +x gdown.pl;
-./gdown.pl https://drive.google.com/file/d/1iQjTh7pRVrBkqB4VlwRJEyTxwOG2QfFQ/view?usp=sharing test_set.hdf5;
-./gdown.pl https://drive.google.com/file/d/1E-Fxg5HLFUuxqNkbQh4kC9lsZPMdg3NL/view?usp=sharing train_validation_set_1.hdf5;
-./gdown.pl https://drive.google.com/file/d/1emp9WVFJ5ctogHNIumKglILgTMqb22O3/view?usp=sharing train_validation_set_2.hdf5;
-./gdown.pl https://drive.google.com/file/d/1BfQYvDrRbttm2IUAgmOfYr6h3oMOqTv8/view?usp=sharing train_validation_set_3.hdf5;
-
+./gdown.pl https://drive.google.com/file/d/1iQjTh7pRVrBkqB4VlwRJEyTxwOG2QfFQ/view?usp=sharing data/test_set.hdf5;
+./gdown.pl https://drive.google.com/file/d/1E-Fxg5HLFUuxqNkbQh4kC9lsZPMdg3NL/view?usp=sharing data/train_validation_set_1.hdf5;
+./gdown.pl https://drive.google.com/file/d/1emp9WVFJ5ctogHNIumKglILgTMqb22O3/view?usp=sharing data/train_validation_set_2.hdf5;
+./gdown.pl https://drive.google.com/file/d/1BfQYvDrRbttm2IUAgmOfYr6h3oMOqTv8/view?usp=sharing data/train_validation_set_3.hdf5;
--- models/get_toy_dataset.sh
+++ models/get_toy_dataset.sh
@@ -1,6 +1,6 @@
+mkdir data;
 chmod +x gdown.pl;
-./gdown.pl https://drive.google.com/file/d/1WdtrMytKh4S3PEo9O51Y0Wv3Cko0Is6S/view?usp=sharing test_set.hdf5;
-./gdown.pl https://drive.google.com/file/d/1nqLlcKXqDtlpeQhksMRsetSrQrI7we3e/view?usp=sharing train_validation_set_1.hdf5;
-./gdown.pl https://drive.google.com/file/d/1V_XxHkUY0dTlkBpFpSw0VhPPNTYU9Q-L/view?usp=sharing train_validation_set_2.hdf5;
-./gdown.pl https://drive.google.com/file/d/1KR0jotLHhUSLxeVOxEkf_VdMRD2UslAu/view?usp=sharing train_validation_set_3.hdf5;
-
+./gdown.pl https://drive.google.com/file/d/1WdtrMytKh4S3PEo9O51Y0Wv3Cko0Is6S/view?usp=sharing data/test_set.hdf5;
+./gdown.pl https://drive.google.com/file/d/1nqLlcKXqDtlpeQhksMRsetSrQrI7we3e/view?usp=sharing data/train_validation_set_1.hdf5;
+./gdown.pl https://drive.google.com/file/d/1V_XxHkUY0dTlkBpFpSw0VhPPNTYU9Q-L/view?usp=sharing data/train_validation_set_2.hdf5;
+./gdown.pl https://drive.google.com/file/d/1KR0jotLHhUSLxeVOxEkf_VdMRD2UslAu/view?usp=sharing data/train_validation_set_3.hdf5;
